c5b3ade27c7f442e87d7b5c9f19c5d2a1a486944
Muskan Singh <76639591+Muskan-25@users.noreply.github.com>
2022-11-12 11:22:04 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,36 @@
+let drum = document.querySelectorAll("button");
+for (var i = 0; i < drum.length; i++) {
+    drum[i].addEventListener("click", function() {
+        var btninnerHTML = this.innerHTML;
+        makesound(btninnerHTML);
+    });
+}
+
+document.addEventListener("keypress", function(event) {
+    makesound(event.key);
+});
+
+function makesound(key) {
+    if (key === "W" || key === "w") {
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+    } else if (key === "a" || key === "A") {
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+    } else if (key === "s" || key === "S") {
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+    } else if (key === "d" || key === "D") {
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+    } else if (key === "j" || key === "J") {
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+    } else if (key === "k" || key === "K") {
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+    } else if (key === "l" || key === "L") {
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+    }
+}
